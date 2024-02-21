41764b3cad3a96e954fd82864cf844203d3f89b6
Tub1005 <tobias.folkmann@hotmail.com>
2024-02-21 22:07:37 +0100

    Data fixed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into SKIN (name, category, skin_Float, rarity, quality, imageurl, stat_trak) VALUES ('Desert Eagle | Meteorite', 1, 0.06426934, 5,1 ,'https://raw.githubusercontent.com/ByMykel/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_deagle_am_crystallized_dark_light_png.png', true);
